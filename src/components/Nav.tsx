9049beeb11c44402edbb54ee5bbd2300b3939e78
Tahsin Tabassum <tahsintabassum207@gmail.com>
2026-09-18 11:13:13 +0600

    first commit

---
+++ src/components/Nav.tsx
@@ -0,0 +1,22 @@
+const Nav =() => {
+return (
+    <nav className="flex items-cneter justify-between">
+<div>
+    <img src="" alt="" />
+</div>
+
+<div>
+    <a href="#home">Home</a>
+    <a href="#home">Home</a>
+    <a href="#home">Home</a>
+    <a href="#home">Home</a>
+    <a href="#home">Home</a>
+</div>
+
+
+    </nav>
+)
+};
+
+
+export default Nav;
