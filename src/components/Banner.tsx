5fd9ce909d6903a113226d5a62ef3f7af8b5c42e
Tahsin Tabassum <tahsintabassum207@gmail.com>
2026-09-18 21:54:33 +0600

    banner done

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,25 +2,23 @@ import bannerimg from "../assets/banner-stack.png";
 
 const Banner = () =>{
 return (
-    <section id="home" className="px-5 py-16 md:px-10 bg-white">
-<div className="mx-auto flex max-w-7xl items-center justify-between">
+    <section id="home" className=" px-8 py-16 md:px-10 bg-white">
+<div className="flex items-center justify-between ">
 {/* left */}
 <div className="w-full md:w-1/2">
 
-    <h1 className="text-5xl font-bold leading-[1.1]">
-   <span className="text-black">Build Your Ideal </span> 
-    <br />
+    <h1>
+   <span className="block text-5xl text-black leading-tight font-bold text-justify px-7">Build Your Ideal </span> 
 
-    <span className=" block bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent" >Development Stack</span>
+    <span className=" bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-5xl font-bold text-justify px-6 text-transparent" >Development Stack</span>
     </h1>
-    <p className="mt-8 max-w-190 text-left text-l leading-7 text-gray-600 ">
-Explore frontend, backend, database, and tooling options,
-compare them side by side, and put together the stack that fits your
+    <p className="max-w-250 text-left px-6 pr-1 text-l text-gray-600 ">
+Explore frontend, backend, database, and tooling options, compare them side by side, and put together the stack that fits your
 next project.
     </p>
 
 {/* button */}
-<div className="mt-8 flex gap-4">
+<div className="mt-8 flex gap-4 px-6 py-2">
     <button className="rounded-xl bg-linear-to-r  from-[#F97316] to-[#EC4899] px-6 py-3 font-semibold text-white">
 Explore Technologies 
 </button>
@@ -29,8 +27,8 @@ Explore Technologies
 </div>
 
 {/* right */}
-<div className="hidden md:block md:w-1/2">
-    <img src={bannerimg} alt="Banner" className="mx-auto w-full max-w-lg" />
+<div className="max-w-full">
+    <img src={bannerimg} alt="Banner" className="absolute right-30 top-1/2 w-125 -translate-y-1/2" />
 </div>
 </div>
     </section>
